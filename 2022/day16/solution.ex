fe80b523dae2967d6d1e445c9f9e9189520862bc
stackcats <stackcats.hk@gmail.com>
2024-01-03 18:18:59 +0800

    2022 day 16

--- 2022/day16/solution.ex
+++ 2022/day16/solution.ex
@@ -1,94 +1,87 @@
 defmodule Solution do
-  use Agent
-
-  defmodule Valve do
-    defstruct [:rate, :ns]
-
-    def new(rate, ns) do
-      %Valve{rate: rate, ns: ns}
-    end
+  defmodule Path do
+    defstruct rate: 0, routes: %MapSet{}
   end
 
   def part1() do
-    init()
-
-    reset()
-
-    vals = input()
-
-    dfs("AA", vals, 30)
+    all_paths(30)
+    |> Enum.max_by(fn path -> path.rate end)
+    |> Map.get(:rate)
   end
 
-  def init() do
-    Agent.start_link(fn -> %{} end, name: __MODULE__)
-  end
+  def part2() do
+    paths = all_paths(26)
 
-  def reset() do
-    Agent.update(__MODULE__, fn _ -> %{} end)
-  end
-
-  def get(k) do
-    Agent.get(__MODULE__, fn cache -> cache[k] end)
-  end
-
-  def update(k, v) do
-    Agent.update(__MODULE__, fn cache -> Map.put(cache, k, v) end)
-  end
-
-  def dfs(curr, vals, ct, opened \\ []) do
-    cache_key = {curr, ct, opened}
-    cache_val = get(cache_key)
-    v = vals[curr]
-
-    cond do
-      cache_val != nil ->
-        cache_val
-
-      ct <= 0 ->
-        0
-
-      curr in opened ->
-        v.ns
-        |> Enum.reduce(0, fn adj, acc ->
-          max(acc, dfs(adj, vals, ct - 1, opened))
-        end)
-        |> then(fn acc ->
-          update(cache_key, acc)
+    for a <- paths, b <- paths, reduce: 0 do
+      acc ->
+        if a.rate + b.rate > acc && MapSet.intersection(a.routes, b.routes) == %MapSet{} do
+          a.rate + b.rate
+        else
           acc
-        end)
-
-      true ->
-        pressure = v.rate * (ct - 1)
-        curr_opened = Enum.sort([curr | opened])
-
-        v.ns
-        |> Enum.reduce(0, fn adj, acc ->
-          p1 = if pressure > 0, do: dfs(adj, vals, ct - 2, curr_opened) + pressure, else: 0
-          p2 = dfs(adj, vals, ct - 1, opened)
-          [p1, p2, acc] |> Enum.max()
-        end)
-        |> then(fn acc ->
-          update(cache_key, acc)
-          acc
-        end)
+        end
     end
+  end
+
+  def all_paths(time) do
+    {rates, graph} = input()
+    dist = floyd_warshall(graph)
+
+    vs =
+      rates |> Enum.filter(fn {_, v} -> v != 0 end) |> Enum.map(fn {k, _} -> k end)
+
+    dfs("AA", vs, time, %Path{}, %MapSet{}, dist, rates)
+  end
+
+  def floyd_warshall(graph) do
+    vs = Map.keys(graph)
+
+    dist =
+      for a <- vs, b <- vs, reduce: %{} do
+        acc ->
+          cond do
+            a == b -> 0
+            b in graph[a] -> 1
+            true -> 99999
+          end
+          |> then(fn n -> Map.put(acc, {a, b}, n) end)
+      end
+
+    for k <- vs, i <- vs, j <- vs, reduce: dist do
+      acc ->
+        n = min(acc[{i, j}], acc[{i, k}] + acc[{k, j}])
+        Map.put(acc, {i, j}, n)
+    end
+  end
+
+  def dfs(curr, vs, remain_time, path, visited, dist, rates) do
+    vs
+    |> Enum.reduce([path], fn v, paths ->
+      remain_time = remain_time - dist[{curr, v}] - 1
+
+      if remain_time <= 0 || MapSet.member?(visited, v) do
+        paths
+      else
+        rate = path.rate + remain_time * rates[v]
+        routes = MapSet.put(path.routes, v)
+        path = %{path | rate: rate, routes: routes}
+        visited = MapSet.put(visited, v)
+        paths ++ dfs(v, vs, remain_time, path, visited, dist, rates)
+      end
+    end)
   end
 
   def input() do
     File.stream!("./input.txt")
     |> Stream.map(&String.trim/1)
     |> Stream.map(fn s ->
-      r =
-        ~r/Valve (?<valve>[A-Z]{2}) has flow rate=(?<rate>\d*); tunnels? leads? to valves? (?<adj>.*)/
-
-      Regex.named_captures(r, s)
-      |> then(fn m ->
-        valve = m["valve"]
-        rate = String.to_integer(m["rate"])
-        adj = String.split(m["adj"], ", ")
-        {valve, Valve.new(rate, adj)}
-      end)
+      [valve | adj] = Regex.scan(~r/[A-Z]{2}/, s) |> Enum.map(&hd/1)
+      rate = Regex.scan(~r/\d+/, s) |> hd() |> hd() |> String.to_integer()
+      {valve, rate, adj}
     end)
-    |> Map.new()
+    |> Enum.reduce({%{}, %{}}, fn {k, rate, lst}, {rates, graph} ->
+      rates = Map.put(rates, k, rate)
+      graph = Map.put(graph, k, lst)
+      {rates, graph}
+    end)
   end
 end
