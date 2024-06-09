88cb0ccfb7fb3ad3e235d5e7c6e7955b81ce6c6c
stackcats <stackcats.hk@gmail.com>
2024-06-09 14:45:02 +0800

    2019 11

--- 2019/day11/solution.ex
+++ 2019/day11/solution.ex
@@ -143,61 +143,6 @@ defmodule Solution do
     Enum.at(lst, i)
   end
 
-  def run(mp, arg, i \\ 0, offset \\ 0) do
-    s = "#{mp[i]}" |> String.pad_leading(5, "0")
-    [a, b, c, d, e] = s |> String.graphemes() |> Enum.map(&String.to_integer/1)
-    op = d * 10 + e
-
-    args = [mp[i + 1] || 0, mp[i + 2] || 0, mp[i + 3] || 0]
-    modes = [c, b, a]
-
-    {reads, writes} =
-      Enum.zip(args, modes)
-      |> Enum.with_index()
-      |> Enum.reduce({%{}, %{}}, fn {{arg, mode}, i}, {reads, writes} ->
-        {
-          Map.put(reads, i, elem({mp[arg], arg, mp[arg + offset]}, mode)),
-          Map.put(writes, i, elem({arg, nil, arg + offset}, mode))
-        }
-      end)
-
-    case op do
-      1 ->
-        mp |> Map.put(writes[2], reads[0] + reads[1]) |> run(arg, i + 4, offset)
-
-      2 ->
-        mp |> Map.put(writes[2], reads[0] * reads[1]) |> run(arg, i + 4, offset)
-
-      3 ->
-        mp |> Map.put(writes[0], arg) |> run(arg, i + 2, offset)
-
-      4 ->
-        {reads[0], mp, i, offset}
-
-      5 ->
-        i = if reads[0] != 0, do: reads[1], else: i + 3
-        run(mp, arg, i, offset)
-
-      6 ->
-        i = if reads[0] == 0, do: reads[1], else: i + 3
-        run(mp, arg, i, offset)
-
-      7 ->
-        v = if reads[0] < reads[1], do: 1, else: 0
-        mp |> Map.put(writes[2], v) |> run(arg, i + 4, offset)
-
-      8 ->
-        v = if reads[0] == reads[1], do: 1, else: 0
-        mp |> Map.put(writes[2], v) |> run(arg, i + 4, offset)
-
-      9 ->
-        run(mp, arg, i + 2, offset + reads[0])
-
-      99 ->
-        :done
-    end
-  end
-
   def input() do
     File.stream!("./input.txt")
     |> Stream.map(&String.trim/1)
